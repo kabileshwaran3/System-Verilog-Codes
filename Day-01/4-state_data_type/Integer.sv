660c187e926b73bb10d58ab68c4f8015aec58fd0
kabileshwaran <skabilesh231@gmail.com>
2025-09-20 12:08:21 +0530

    Create Integer.sv

---
+++ Day-01/4-state_data_type/Integer.sv
@@ -0,0 +1,18 @@
+`timescale 1ns / 1ps
+module integer_ex;
+    integer count;
+    initial begin
+        count =5;
+     repeat (count) begin
+            $display("count = %0d", count);
+            count =count -1;
+        end
+    end
+endmodule
+
+//output
+//count = 5
+//count = 4
+//count = 3
+//count = 2
+//count = 1

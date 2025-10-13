612c3d0f8b2e5972243d1747fae86b8321d00ce4
kabileshwaran <skabilesh231@gmail.com>
2025-10-13 18:17:59 +0530

    Update type_def_ex.sv

--- Day-02/Union/Packed/type_def_ex.sv
+++ Day-02/Union/Packed/type_def_ex.sv
@@ -1,8 +1,7 @@
-// Code your design here
 module packed_union;
-  typedef union packed{ reg[5:0]roll_num;
-  				logic[5:0]sub_mark;
-               reg[5:0]total_mark;}student_detail;
+  typedef union packed{ reg[7:0]roll_num;
+                       logic[7:0]sub_mark;
+                       reg[7:0]total_mark;}student_detail;
   initial begin
     student_detail m1,m2;
     m1.roll_num=3;
@@ -17,5 +16,6 @@ module packed_union;
   end
 endmodule
 
-# Packed Student-1 Detail='{roll_num:11, sub_mark:11, total_mark:11}
+# run -all
+# Packed Student-1 Detail='{roll_num:75, sub_mark:75, total_mark:75}
 # Packed Student-2 Detail='{roll_num:55, sub_mark:55, total_mark:55}

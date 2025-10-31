b48648db46b4d962c7d50af3435023d99609295a
kabileshwaran <skabilesh231@gmail.com>
2025-10-31 09:57:11 +0530

    Update function_ex1.sv

--- 03.Task_and_Function/Function/function_ex1.sv
+++ 03.Task_and_Function/Function/function_ex1.sv
@@ -1,1 +1,16 @@
+// Code your design here
+module sim_fun;
+  int res;
+  
+  initial begin
+   res= perform(10,10);
+    $display("call By Function %0d ",res);
+  end
+             
+  function int perform(int var_1,var_2);
+    return var_1*var_2;
+  endfunction
+endmodule
+
+# call By Function 100 
 

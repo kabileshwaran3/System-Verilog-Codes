732b4aa5687391e02030b6a3906fe70e74f12559
kabileshwaran <skabilesh231@gmail.com>
2025-11-05 16:22:08 +0530

    Create ex1.sv

---
+++ 05.Statements_and_Loops/Statement/ex1.sv
@@ -0,0 +1,15 @@
+module if_example;  
+  initial begin
+    int a, b;
+    a = 10;
+    b = 20;
+    if(a>b)
+      $display("a is greater than b");
+    else if(a<b)
+      $display("a is less than b");
+    else 
+      $display("a is equal to b");
+  end
+endmodule
+
+# a is less than b

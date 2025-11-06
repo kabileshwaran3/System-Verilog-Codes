c89b33b85a42f93778bef18f0e2263428c3f3a0d
kabileshwaran <skabilesh231@gmail.com>
2025-11-06 10:54:26 +0530

    Create repeat_ex.sv

---
+++ 05.Statements_and_Loops/Loops/repeat_ex.sv
@@ -0,0 +1,14 @@
+// Code your design here
+module repeat_ex;
+  initial begin
+    repeat(5)begin
+      $display("REPEAT LOOP");
+    end
+  end
+endmodule
+
+# REPEAT LOOP
+# REPEAT LOOP
+# REPEAT LOOP
+# REPEAT LOOP
+# REPEAT LOOP

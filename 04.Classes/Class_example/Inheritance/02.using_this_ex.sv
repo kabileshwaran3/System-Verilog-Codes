81bbb383814e618c7226e767e61e3cdebdbdf104
kabileshwaran <skabilesh231@gmail.com>
2025-11-06 14:11:54 +0530

    Create 02.using_this_ex.sv

---
+++ 04.Classes/Class_example/Inheritance/02.using_this_ex.sv
@@ -0,0 +1,21 @@
+// Code your design here
+class ex;
+  bit[31:0]data;
+  int value;
+  
+  function new(bit [31:0]data,int value);
+    this.data=data;
+    this.value=value;
+  endfunction
+endclass
+
+module assi;
+  ex p1;
+  initial begin
+    p1=new(150,30);
+   $display("The data =%0d,The value =%0d",p1.data,p1.value);
+  end
+endmodule
+
+
+# The data =150,The value =30

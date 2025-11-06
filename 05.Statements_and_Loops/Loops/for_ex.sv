1bafd581b81b02f9711bf5ea7ef482065935cb60
kabileshwaran <skabilesh231@gmail.com>
2025-11-06 09:50:00 +0530

    Create for_ex.sv

---
+++ 05.Statements_and_Loops/Loops/for_ex.sv
@@ -0,0 +1,21 @@
+module exam;
+  int i;
+  initial begin
+    for(int i=0; i<=10; i++)
+      $display("The Value Of i =%0d",i);
+    
+  end
+endmodule
+
+
+# The Value Of i =0
+# The Value Of i =1
+# The Value Of i =2
+# The Value Of i =3
+# The Value Of i =4
+# The Value Of i =5
+# The Value Of i =6
+# The Value Of i =7
+# The Value Of i =8
+# The Value Of i =9
+# The Value Of i =10

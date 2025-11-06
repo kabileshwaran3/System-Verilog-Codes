c4f22d16188ac2d152dbaeb119cd5dd242cb2a40
kabileshwaran <skabilesh231@gmail.com>
2025-11-06 09:10:52 +0530

    Update and rename ex1.sv to while_ex1.sv

---
+++ 05.Statements_and_Loops/Loops/while_ex1.sv
@@ -0,0 +1,24 @@
+
+// Code your design here
+module ex;
+  int i;
+  initial begin
+    while(i<=10)begin
+      $display("value of i = %0d",i);
+      i++;
+    end
+  end
+endmodule
+
+
+# value of i = 0
+# value of i = 1
+# value of i = 2
+# value of i = 3
+# value of i = 4
+# value of i = 5
+# value of i = 6
+# value of i = 7
+# value of i = 8
+# value of i = 9
+# value of i = 10

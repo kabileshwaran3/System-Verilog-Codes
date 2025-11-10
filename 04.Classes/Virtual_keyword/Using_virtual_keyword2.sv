da7bc3c40c0ce576a533792b2fd9a0e8018ed5cd
kabileshwaran <skabilesh231@gmail.com>
2025-11-10 15:53:39 +0530

    Create Using_virtual_keyword2.sv

---
+++ 04.Classes/Virtual_keyword/Using_virtual_keyword2.sv
@@ -0,0 +1,47 @@
+class base;
+  int data;
+  bit[15:0]addrs;
+  virtual function void display();
+    $display("Display from Base class");
+    $display("Data = %0d , Addrs =%0d",data,addrs);
+  endfunction
+endclass
+
+class child_1 extends base;
+ virtual function void display();
+    $display("Display from first Child class");
+    $display("Data = %0d , Addrs =%0d",data,addrs);    
+  endfunction
+endclass
+
+class child_2 extends child_1;
+  function void display();
+    $display("Display from Secound Child class");
+    $display("Data = %0d , Addrs =%0d",data,addrs);
+  endfunction
+endclass
+
+module ex;
+  base p1;
+  child_1 c1;
+  child_2 c2;
+  initial begin
+    c1=new();
+    c2=new();
+    
+    p1=c1;
+    p1.data=21;
+    p1.addrs=199;
+    p1.display();
+    c1=c2;
+    c1.data=31;
+    c1.addrs=299;
+    c1.display();
+    
+  end
+endmodule
+    
+# Display from first Child class
+# Data = 21 , Addrs =199
+# Display from Secound Child class
+# Data = 31 , Addrs =299

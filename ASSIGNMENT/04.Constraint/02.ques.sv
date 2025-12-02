5d789a38aedf760b92259f24f4e23399b0941b3f
kabileshwaran <skabilesh231@gmail.com>
2025-12-02 22:00:33 +0530

    Update 02.ques.sv

--- ASSIGNMENT/04.Constraint/02.ques.sv
+++ ASSIGNMENT/04.Constraint/02.ques.sv
@@ -22,3 +22,30 @@ endmodule
 # DATA = 40
 # DATA = 36
 # DATA = 36
+
+METHOD 2
+
+// Write a constraint to generate specific values: 25, 27, 30, 36, 40, 45.
+                
+class range;
+  rand int data;
+endclass
+
+module ex;
+  range r1;
+  initial begin
+    repeat (5)begin
+    r1=new();
+    r1.randomize() with {data inside {25,27,30,36,40,45};};
+    $display("DATA = %0d",r1.data);
+  end
+  end
+endmodule
+
+
+# DATA = 25
+# DATA = 30
+# DATA = 45
+# DATA = 27
+# DATA = 30
+                        

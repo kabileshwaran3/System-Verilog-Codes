5e898a7777766c58dd763484d13694c836788d6c
kabileshwaran <skabilesh231@gmail.com>
2025-12-04 11:22:24 +0530

    Create 05.fork_join_none.sv

---
+++ 08.Threads/05.fork_join_none.sv
@@ -0,0 +1,31 @@
+module base;
+  initial begin
+    $display($time ," process 1 is executed");
+    #20
+    $display($time, " process 2 is executed");
+    fork
+      begin
+        $display($time, " statement 1 is executed");
+        #30
+        $display($time ," statement 1 is completed");
+      end
+      begin
+        $display($time, " statement 2 is executed");
+        #30
+        $display($time ," statement 2 is completed");
+      end
+    join_none;
+    $display($time, " outside fork _none block");
+    #20
+    $display("--------------------------");
+  end
+endmodule
+
+#                    0 process 1 is executed
+#                   20 process 2 is executed
+#                   20 outside fork _none block
+#                   20 statement 1 is executed
+#                   20 statement 2 is executed
+# --------------------------
+#                   50 statement 1 is completed
+#                   50 statement 2 is completed
